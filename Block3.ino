faacb0902b1e7949d4a4a0b20ef0c86bf8e243b2
gablmao <88683534+gablmao@users.noreply.github.com>
2023-03-12 19:10:05 +0000

    Add files via upload

---
+++ Block3.ino
@@ -0,0 +1,130 @@
+#include <LiquidCrystal.h>
+
+#define rockButton 9
+#define paperButton 8
+#define scissorsButton 7
+#define finalChoice 6
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int player1Score = 0;
+int player1Final = 0;
+int player2Score = 0;
+int player2Final = 0;
+int state = 0;
+
+const int win_matrix[3][3] = {
+  // Column = Player1 chose rock, paper or scissors
+  // Row = Player2 chose rock paper or scissors
+  { 0,                  1,                  -1 },
+  { -1,                 0,                   1 }, 
+  { 1,                  -1,                  0 }
+};
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(rockButton, INPUT);
+  pinMode(paperButton, INPUT);
+  pinMode(scissorsButton, INPUT);
+  pinMode(finalChoice, INPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+}
+
+
+
+// game functions
+void player1Wins(void)
+{
+  player1Score += 1
+  lcd.setCursor(0,0);
+  lcd.print("Player 1 Wins!")
+  lcd.setCursor(0,1);
+  lcd.print("POGGERS")
+}
+
+void player2Wins(void)
+{
+  player2Score += 1
+  lcd.setCursor(0,0);
+  lcd.print("Player 2 Wins!")
+  lcd.setCursor(0,1);
+  lcd.print("POGGERS")
+}
+
+void playersTied(void)
+{
+  lcd.setCursor(0,0);
+  lcd.print("You tied!")
+  lcd.setCursor(0,1);
+  lcd.print("No winners LMAOO")
+}
+
+void displayScores(void)
+{
+  lcd.setCursor(0,0);
+  lcd.print(player1Score);
+  lcd.setCursor(0,1);
+  lcd.print(player2Score);
+}
+
+
+
+//game loop
+void loop()
+{
+  if (state == 0) // Player 1 makes a choice
+  {
+    lcd.print("pick your poison guzzo1")
+
+    // finalChoice button stores answer to variable player1Final 
+
+    while (!digitalRead(finalChoice)) {} // Wait for button press
+    player1Final = (digitalRead(rockButton) == HIGH) ? 0 : (digitalRead(paperButton) == HIGH) ? 1 : 2; // Map button press to choice (0 for rock, 1 for paper, 2 for scissors)
+
+    state == 1;
+  }
+
+  if (state == 1) // Player 2 makes a choice
+  {
+    lcd.print("pick your poison guzzo2")
+    
+    // finalChoice button stores answer to variable player2Final  
+
+    while (!digitalRead(finalChoice)) {} // Wait for button press
+    player2Final = (digitalRead(rockButton) == HIGH) ? 0 : (digitalRead(paperButton) == HIGH) ? 1 : 2; // Map button press to choice (0 for rock, 1 for paper, 2 for scissors)
+    state == 2;
+  }
+
+  if state == 2; // Check who the winner is
+  {
+    // if check player1 Win == True
+    // player1Wins();
+
+    // else if check player2 wins == True
+    // player2Wins();
+
+    // else
+    // playersTied();
+
+    int outcome = win_matrix[player1Final][player2Final];  // Determines the winner using the matrix
+    if (outcome == 1) {
+      player1Wins();
+      delay(5000);
+    } else if (outcome == -1) {
+      player2Wins();
+      delay(5000);
+    } else { // (outcome == 0)
+      playersTied();
+      delay(5000);
+    }
+
+
+    displayScores();
+    delay(5000);  // After 5 seconds, the game starts again
+    state = 0;  // This loops back to the beginning
+  }
+}
