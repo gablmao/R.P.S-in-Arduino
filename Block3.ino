0a9573ac2fc1613ffc1a0b0cf874be5764e0467f
gablmao <88683534+gablmao@users.noreply.github.com>
2023-03-13 09:13:06 +0000

    updated "player1/2Final" variable (lines 109/126)
    2 doesn't exactly exist in "win_matrix", missed that. should've been -1 instead.
    next changes should be to add some debounce on the push button "finalChoice".

--- Block3.ino
+++ Block3.ino
@@ -8,14 +8,24 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int player1Score = 0;
-int player1Final = 0;
+unsigned int player1Final = 0;
 int player2Score = 0;
-int player2Final = 0;
+unsigned int player2Final = 0;
 int state = 0;
 
+
+
+//add delay after pressing the finalChoice button
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+bool buttonState;
+bool lastButtonState = LOW;
+
+
+
 const int win_matrix[3][3] = {
-  // Column = Player1 chose rock, paper or scissors
-  // Row = Player2 chose rock paper or scissors
+  // Column = Player1 choose rock, paper or scissors
+  // Row = Player2 choose rock paper or scissors
   { 0,                  1,                  -1 },
   { -1,                 0,                   1 }, 
   { 1,                  -1,                  0 }
@@ -39,28 +49,28 @@ void setup()
 // game functions
 void player1Wins(void)
 {
-  player1Score += 1
+  player1Score += 1;
   lcd.setCursor(0,0);
-  lcd.print("Player 1 Wins!")
+  lcd.print("Player 1 Wins!");
   lcd.setCursor(0,1);
-  lcd.print("POGGERS")
+  lcd.print("POGGERS");
 }
 
 void player2Wins(void)
 {
-  player2Score += 1
+  player2Score += 1;
   lcd.setCursor(0,0);
-  lcd.print("Player 2 Wins!")
+  lcd.print("Player 2 Wins!");
   lcd.setCursor(0,1);
-  lcd.print("POGGERS")
+  lcd.print("POGGERS");
 }
 
 void playersTied(void)
 {
   lcd.setCursor(0,0);
-  lcd.print("You tied!")
+  lcd.print("You tied!");
   lcd.setCursor(0,1);
-  lcd.print("No winners LMAOO")
+  lcd.print("No winners LMAOO");
 }
 
 void displayScores(void)
@@ -73,33 +83,53 @@ void displayScores(void)
 
 
 
+// add debounce button function here
+
+int readPlayerFinalChoice(void)
+{
+  
+}
+
 //game loop
 void loop()
 {
+  
+  
+  
   if (state == 0) // Player 1 makes a choice
   {
-    lcd.print("pick your poison guzzo1")
-
+    lcd.setCursor(0,0);
+    lcd.print("pick your poison");
+    lcd.setCursor(0,1);
+    lcd.print("guzzo 1");
+	
     // finalChoice button stores answer to variable player1Final 
-
-    while (!digitalRead(finalChoice)) {} // Wait for button press
-    player1Final = (digitalRead(rockButton) == HIGH) ? 0 : (digitalRead(paperButton) == HIGH) ? 1 : 2; // Map button press to choice (0 for rock, 1 for paper, 2 for scissors)
-
-    state == 1;
+    while (digitalRead(finalChoice) == LOW) 
+    {
+      player1Final = (digitalRead(rockButton) == HIGH) ? 0 : (digitalRead(paperButton) == HIGH) ? 1 : -1; // Map button press to choice (0 for rock, 1 for paper, -1 for scissors)
+    }
+    
+    state = 1;
   }
 
   if (state == 1) // Player 2 makes a choice
   {
-    lcd.print("pick your poison guzzo2")
+    lcd.setCursor(0,0);
+    lcd.print("pick your poison");
+    lcd.setCursor(0,1);
+    lcd.print("guzzo 2");
     
     // finalChoice button stores answer to variable player2Final  
+	
+    while (!digitalRead(finalChoice)) 
+    {
+      player2Final = (digitalRead(rockButton) == HIGH) ? 0 : (digitalRead(paperButton) == HIGH) ? 1 : -1;
+    }
 
-    while (!digitalRead(finalChoice)) {} // Wait for button press
-    player2Final = (digitalRead(rockButton) == HIGH) ? 0 : (digitalRead(paperButton) == HIGH) ? 1 : 2; // Map button press to choice (0 for rock, 1 for paper, 2 for scissors)
-    state == 2;
+    state = 2;
   }
 
-  if state == 2; // Check who the winner is
+  if (state == 2) // Check who the winner is
   {
     // if check player1 Win == True
     // player1Wins();
@@ -111,16 +141,13 @@ void loop()
     // playersTied();
 
     int outcome = win_matrix[player1Final][player2Final];  // Determines the winner using the matrix
-    if (outcome == 1) {
+    if (outcome == -1) 
       player1Wins();
-      delay(5000);
-    } else if (outcome == -1) {
+    else if (outcome == 1) 
       player2Wins();
-      delay(5000);
-    } else { // (outcome == 0)
+    else // (outcome == 0)
       playersTied();
-      delay(5000);
-    }
+    
 
 
     displayScores();
